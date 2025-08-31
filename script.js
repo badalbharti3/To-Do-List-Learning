de227d31f2a897f1d17fb16c02b0f8dec1e08f6d
Badal <badalbharti5@gmail.com>
2025-08-31 23:29:06 +0530

    Some Changes

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let c = 1;
+let taskArray = [];
 document.getElementById("add-btn").addEventListener("click", function () {
   let p = document.getElementById("todo-input");
   if (p.value === "") {
     alert("Enter any task.");
     return;
   }
-  let newTask = document.createElement("li");
-  let newbtn = document.createElement("button");
-  newbtn.textContent = "Delete";
-  newTask.textContent = `Task ${c++}: ${p.value} `;
-  document.getElementById("todolist").appendChild(newTask);
-  document.getElementById("todolist").lastChild.appendChild(newbtn);
+  
+  if(!taskArray.includes(p.value)){
+    taskArray.push(p.value);
+    let newTask = document.createElement("li");
+    let newbtn = document.createElement("button");
+    newbtn.textContent = "Delete";
+    
+    newTask.textContent = `Task ${c++}: ${p.value} `;
+    document.getElementById("todolist").appendChild(newTask);
+    document.getElementById("todolist").lastChild.appendChild(newbtn);
 
-  p.value = "";
+    p.value = "";
+  }
+  else alert('Task already on the list!!!')
+  
 });
